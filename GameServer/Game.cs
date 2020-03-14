f091eceabd878acc88f0a6a5253a1336904a2a28
Dor Green <dor@cdi-negev.com>
2020-03-14 17:28:21 +0200

    GameServer: GameState is handled with State Design Pattern ; every state has "HandlePacket". Lots of logic is missing

--- GameServer/Game.cs
+++ GameServer/Game.cs
@@ -5,76 +5,204 @@ using Lidgren.Network;
 
 namespace GameServer
 {
-    public enum GameStatus
+    
+    
+    public abstract class GameState
     {
-        AwaitConnection,
-        AwaitBoardInitialization,
-        AwaitPlayerStart,
-        GameWaitFirstPlayerMove,
-        GameWaitSecondPlayerMove,
-        GameOver
+        
+        
+        
+        protected Game game;
+        public abstract void HandlePacket(NetIncomingMessage msg);
+
+        public GameState(Game game)
+        {
+            this.game = game;
+        }
+        
     }
+
+    public class AwaitConnectionState : GameState
+    {
+        public AwaitConnectionState(Game game) : base(game)
+        {
+        }
+
+        // Register new players if appilcable
+        // Deny any other request
+        // Move to awaitboardinit when two players are present
+        public override void HandlePacket(NetIncomingMessage message)
+        {
+            if(message == null) return;
+             
+            switch (message.MessageType)
+            {
+                case NetIncomingMessageType.DebugMessage:
+                case NetIncomingMessageType.ErrorMessage:
+                case NetIncomingMessageType.WarningMessage:
+                case NetIncomingMessageType.VerboseDebugMessage:
+                    string text = message.ReadString();
+                    Console.WriteLine(String.Format("Got msg:{0}", text));
+                    break;
+                case NetIncomingMessageType.StatusChanged:
+                    NetConnectionStatus status = (NetConnectionStatus)message.ReadByte();
+                    string reason = message.ReadString();
+                    Console.WriteLine(NetUtility.ToHexString(message.SenderConnection.RemoteUniqueIdentifier) + " " + status + ": " + reason);
+
+                    var response = game.server.CreateMessage();
+                    if (status == NetConnectionStatus.Connected)
+                        response = game.players.SignUp(message.SenderConnection, response);
+                    else if (status == NetConnectionStatus.Disconnecting)
+                        response = game.players.SignOut(message.SenderConnection, response);
+
+                    game.server.SendMessage(response, message.SenderConnection, NetDeliveryMethod.ReliableOrdered);
+                    break;
+                default:
+                    break;
+            }
+
+            if (game.players.GetConnection(Ownership.FirstPlayer) != null &&
+                game.players.GetConnection(Ownership.SecondPlayer) != null)
+            {
+                game.state = new AwaitBoardInitializationState(game);
+            }
+        }
+    }
+
+    public class AwaitBoardInitializationState : GameState
+    {
+
+        public bool[] boardinit;
+        public AwaitBoardInitializationState(Game game) : base(game)
+        {
+            // Send both players a notice to init their boards
+            this.boardinit = new[] {false, false};
+        }
+        
+        public override void HandlePacket(NetIncomingMessage msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AwaitPlayerStartState : GameState
+    {
+        public bool[] playerstart;
+
+        public AwaitPlayerStartState(Game game) : base(game)
+        {
+            // Send both players a notice to press "Start Game"
+            this.playerstart = new[] {false, false};   
+        }
+        
+       // When player i sends Start, tell player i to ack
+       // When both players sent start, Move to state GameWaitFirstPlayerMoveState
+        public override void HandlePacket(NetIncomingMessage msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // Should probably be the same as wait for player2 move 
+    // 
+    public class GameWaitFirstPlayerMoveState : GameState
+    {
+        public GameWaitFirstPlayerMoveState(Game game) : base(game)
+        {
+        }
+        
+        public override void HandlePacket(NetIncomingMessage msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    public class GameWaitSecondPlayerMoveState : GameState
+    {
+        public GameWaitSecondPlayerMoveState(Game game) : base(game)
+        {
+        }
+        
+        public override void HandlePacket(NetIncomingMessage msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GameOverState : GameState
+    {
+        public GameOverState(Game game) : base(game)
+        {
+        }
+        
+        public override void HandlePacket(NetIncomingMessage msg)
+        {
+            throw new NotImplementedException();
+        } 
+    }
+    
+    
+    
+    
     
     public class Game
     {
         public Players players;
         private ServerBoard board;
-        public Ownership turn;
-        public GameStatus status;
+        public GameState state;
         public NetServer server;
-        public bool[] boardinit;
-        public bool[] playerstart;
-        
 
 
         public Game()
         {
             this.players = new Players();
             this.board = new ServerBoard();
-            this.turn = Ownership.FirstPlayer;
-            this.status = GameStatus.AwaitConnection;
+            this.state = new AwaitConnectionState(this);
             this.server = null;
-            this.boardinit = new[] {false, false};
-            this.playerstart = new[] {false, false};
         }
 
-        public Game(NetServer server)
+        public Game(NetServer server, Players players)
         {
-            this.players = new Players();
+            this.players = players;
             this.board = new ServerBoard();
-            this.turn = Ownership.FirstPlayer;
-            this.status = GameStatus.AwaitConnection;
+            this.state = new AwaitConnectionState(this);
             this.server = server;
-            this.boardinit = new[] {false, false};
-            this.playerstart = new[] {false, false};
+        }
+
+        public void Reset()
+        {
+            this.board = new ServerBoard();
+            // TODO: Maybe some other state according to Client?
+            this.state = new AwaitBoardInitializationState(this);
         }
 
         public void PrintState()
         {
-            Console.WriteLine("GameState: {0}", this.status.ToString());
-            // TODO: Add "if verbose"
-            switch (this.status)
-            {
-                case GameStatus.AwaitConnection:
-                    Console.WriteLine(this.players.ToString());
-                    break;
-                case GameStatus.AwaitBoardInitialization:
-                    Console.WriteLine("FirstPlayer: {0}, SecondPlayer: {1}", this.boardinit);
-                    break;
-                case GameStatus.AwaitPlayerStart:
-                    Console.WriteLine("FirstPlayer: {0}, SecondPlayer: {1}", this.playerstart);
-                    break;
-                case GameStatus.GameOver:
-                    Console.WriteLine("Winner is: NOTYETIMPLEMENTED");
-                    break;
-                case GameStatus.GameWaitFirstPlayerMove:
-                case GameStatus.GameWaitSecondPlayerMove:
-                    Console.WriteLine("No more info");
-                    break;
-                default:
-                    Console.WriteLine("Unexpected status");
-                    break;
-            }
+            
+            // Console.WriteLine("GameState: {0}", this.status.ToString());
+            // // TODO: Add "if verbose"
+            // switch (this.status)
+            // {
+            //     case GameStatus.AwaitConnection:
+            //         Console.WriteLine(this.players.ToString());
+            //         break;
+            //     case GameStatus.AwaitBoardInitialization:
+            //         Console.WriteLine("FirstPlayer: {0}, SecondPlayer: {1}", this.boardinit);
+            //         break;
+            //     case GameStatus.AwaitPlayerStart:
+            //         Console.WriteLine("FirstPlayer: {0}, SecondPlayer: {1}", this.playerstart);
+            //         break;
+            //     case GameStatus.GameOver:
+            //         Console.WriteLine("Winner is: NOTYETIMPLEMENTED");
+            //         break;
+            //     case GameStatus.GameWaitFirstPlayerMove:
+            //     case GameStatus.GameWaitSecondPlayerMove:
+            //         Console.WriteLine("No more info");
+            //         break;
+            //     default:
+            //         Console.WriteLine("Unexpected status");
+            //         break;
+            // }
         }
         
         
--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -20,46 +20,20 @@ namespace GameServer
             // TODO: Get port from constant or from commandline args
             var config = new NetPeerConfiguration("StrategoLAN") {Port = 11112};
             var server = new NetServer(config);
-            var game = new Game(server);
+            var players = new Players();
+            var game = new Game(server, players);
             
             game.server.Start();
             
             
             NetIncomingMessage message;
-            NetOutgoingMessage response;
             
-
             // TODO: while(true) Should be replaced with Application.Idle handler!
             // TOOD: Test logging out
             while (true)
             {
                 if((message = server.ReadMessage()) == null) continue;
-                switch (message.MessageType)
-                {
-                    case NetIncomingMessageType.DebugMessage:
-                    case NetIncomingMessageType.ErrorMessage:
-                    case NetIncomingMessageType.WarningMessage:
-                    case NetIncomingMessageType.VerboseDebugMessage:
-                        string text = message.ReadString();
-                        Output(String.Format("Got msg:{0}", text));
-                        break;
-                    case NetIncomingMessageType.StatusChanged:
-                        NetConnectionStatus status = (NetConnectionStatus)message.ReadByte();
-                        string reason = message.ReadString();
-                        Output(NetUtility.ToHexString(message.SenderConnection.RemoteUniqueIdentifier) + " " + status + ": " + reason);
-
-                        response = server.CreateMessage();
-                        if (status == NetConnectionStatus.Connected)
-                            response = game.players.SignUp(message.SenderConnection, response);
-                        else if (status == NetConnectionStatus.Disconnecting)
-                            response = game.players.SignOut(message.SenderConnection, response);
-
-                        server.SendMessage(response, message.SenderConnection, NetDeliveryMethod.ReliableOrdered);
-                        break;
-                    default:
-                        break;
-                        
-                }
+                
             }
             
             // int x = 0;
